447760093d40da7a6d147fa6dda345a39959eef6
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2025-04-20 19:33:19 -0300

    refactor: correct app module folder and improve logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  const logger = new Logger('Bootstrap');
+
+  app.enableShutdownHooks();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
-bootstrap();
+
+void bootstrap();
